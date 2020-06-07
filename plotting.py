8b03d84b2b6ee5adbe846b6cb40f3a1cc3a7aa33
Nathan Neibauer <nneibauer044@gmail.com>
2020-06-07 01:22:37 -0600

    Update ribbon_plot with new parameters

--- plotting.py
+++ plotting.py
@@ -4,24 +4,34 @@ from cycler import cycler
 import numpy as np
 
 def ribbon_plot(depths,
+                element_filter,
                 filter_by='Cu',
-                ax=None,
+                combine_scans=True,
+                combine_detsums=True,
                 N=8,
-                normalize_by='counts'):
+                normalize_by='counts',
+                prop_dict=None):
   '''Shows fractional concentration of element among different groups.
 
   Args:
     filter_by: string. Element under investigation
-    ax: plt.Axes. Optional Axes to plot on
+    element_filter: dict specifying how elements should be filtered. For more 
+      info, see `Depth.apply_element_filter`. In Colab, this can be done by
+      opening a new cell and running `Depth.apply_element_filter?`.
+    combine_scans: bool. Whether or not to show individual scans as different
+      bars in the ribbon plot. If this is True, then scans will be combined
+    combine_detsums: bool. Whether or not to combine detsums when applying the
+      filter functions defined in `element_filter`. If this is True, then all
+      detsums within a given depth will be considered when applying the filter.
     N: int. The amount of groups to use. For example, N=12 means "take the
       top 12 groups from each depth"
     normalize_by: string. Which quantity to use for data normalization. This
       can either be 'counts' or 'pixels'.
+    prop_dict: optional dictionary to store properties in for the whole notebook.
+      If this is None, then properties will be created for each individual plot.
   '''
-  if ax is None:
-    fig, ax = plt.subplots(figsize=(16, 4))
-  else:
-    fig = ax.figure()
+
+  fig, ax = plt.subplots(figsize=(16, 4))
 
   # Sort depth objects by depth
   depths = sorted(depths, key=lambda d: int(d.depth[:-1]), reverse=True)
@@ -39,45 +49,51 @@ def ribbon_plot(depths,
   # https://matplotlib.org/cycler/
   props = iter(pattern_cycler * color_cycler)
 
+  if prop_dict is None:
+    prop_dict = {}
 
-  prop_dict = {}
+  if normalize_by == 'pixels':
+    sort_by = 'num_pixels'
+    col = 'num_pixels'
+    normalization = lambda g: g['num_pixels'].sum()
+  elif normalize_by == 'counts':
+    sort_by = f'{filter_by}_counts'
+    col = f'{filter_by}_counts'
+    normalization = lambda g: g[f'{filter_by}_counts'].sum()
+  else:
+    raise ValueError("`normalize_by` must be 'pixels' or 'counts'")
 
-  for i, depth in enumerate(depths):
-    scan = depth.combined_scan
-    # Get the unique groups and the total pixels and counts
-    if normalize_by == 'pixels':
-      sort_by = 'num_pixels'
-      col = 'num_pixels'
-      normalization = lambda g: g['num_pixels'].sum()
-    elif normalize_by == 'counts':
-      sort_by = f'{filter_by}_counts'
-      col = f'{filter_by}_counts'
-      normalization = lambda g: g[f'{filter_by}_counts'].sum()
-    else:
-      raise ValueError("`normalize_by` must be 'pixels' or 'counts'")
-
+  def get_scan_groups(scan):
     groups = scan.filter_by(filter_by).get_unique_groups(elements_to_sum=[filter_by], sort_by=sort_by)
-    groups[col] /= normalization(groups)
+    if len(groups.index):
+      groups[col] /= normalization(groups)
+      groups = groups.iloc[:N]
+    return groups
 
-    # Gets the top N unique groups for this scan
-    groups = groups.iloc[:N]
-
-    # Assigns a unique color for each group, if the color doesn't exist yet
+  # Assigns a unique color for each group, if the color doesn't exist yet
+  def update_prop_dict(groups):
     for g in groups.index:
       if g not in prop_dict:
         prop_dict[g] = next(props)
         prop_dict[g].update({'alpha': 0.8, 'fill': True, 'edgecolor': 'k'})
-        #print(f'Adding {g} to prop_dict')
+      #print(f'Adding {g} to prop_dict')
 
+  # Plots single scan (one bar)
+  def plot_scan(scan, i):
+    groups = get_scan_groups(scan)
+
+    # If groups are empy, do nothing
+    if not len(groups.index):
+      return
+    update_prop_dict(groups)
     left = 0
     thickness = 1
-
     for group in groups.index:
       #Encode the group value (e.g. Cu|Mn -> 0.0778) as the rectangle width
       rect_width = groups[col].loc[group]
       ax.add_patch(mpatches.Rectangle((left, i-thickness/2),
-                             rect_width, thickness,
-                             **prop_dict[group]))
+                            rect_width, thickness,
+                            **prop_dict[group]))
       left += rect_width
 
     t=ax.text(left+0.02, i, f'{col}: {groups[col].sum():0.2f}',
@@ -86,6 +102,22 @@ def ribbon_plot(depths,
             color='black',
             fontweight='bold',
             transform=ax.transData)
+
+  i=0
+  yticklabels = []
+  for depth in depths:
+    depth.apply_element_filter(element_filter, combine_detsums=combine_detsums)
+    if combine_scans:
+      scans = [depth.combined_scan]
+      yticklabels.append(depth.depth)
+    else:
+      scans = depth.scans
+
+    for scan in scans:
+      plot_scan(scan, i)
+      i += 1
+      if not combine_scans:
+        yticklabels.append(f'{depth.depth}: {scan.name}')
 
     # https://stackoverflow.com/questions/23696898/adjusting-text-background-transparency
     #t.set_bbox({'facecolor':'white', 'alpha':0.8})
@@ -106,9 +138,12 @@ def ribbon_plot(depths,
     patch.set_height(22)
     patch.set_y(-10)
   
-  ax.set_yticks(np.arange(0, len(depths)))
-  ax.set_yticklabels([d.depth for d in depths])
-  ax.set_ylim(-0.5, len(depths)-0.5)
+  ax.set_yticks(np.arange(0, i))
+
+  ax.set_yticklabels(yticklabels)
+
+
+  ax.set_ylim(-0.5, i-0.5)
   ax.set_xlim(0, 1.13)
   ax.set_title(f'{filter_by} {normalize_by}', fontsize=16, fontweight='bold')
   plt.show()
